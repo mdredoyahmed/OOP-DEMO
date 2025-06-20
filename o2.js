92d1960908517b3d94e9439a42dbaceaf9922ff4
Md Hridoy Ahmed <95656155+mdredoyahmed@users.noreply.github.com>
2025-06-20 12:42:14 +0600

    learn class

---
+++ o2.js
@@ -0,0 +1,22 @@
+// oop demo 
+
+
+class Person{
+  constructor(fname , lname){
+      this.firsst_name = fname ;
+      this.last_name = lname ;
+  }
+  getFullName(){
+    return this.firsst_name + " " + this.last_name ;
+  }
+}
+
+const jhon = new Person("sumit vai ");
+jhon.last_name = "shakil" ;
+
+const ahmed = new Person("meg" , "balika") ;
+const Hridoy = new Person("kuddos" , "molla") ;
+console.log(ahmed.getFullName());
+console.log(Hridoy.getFullName());
+
+console.log(jhon.getFullName());
